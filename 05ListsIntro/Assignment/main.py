e2f95b94d03437e199e8bf18134741c5d239c1f3
Finn Lehl <27finnlehl@usd266.com>
2023-12-05 09:11:16 -0600

    did work

--- 05ListsIntro/Assignment/main.py
+++ 05ListsIntro/Assignment/main.py
@@ -1,7 +1,9 @@
+import random
+
 def make_abc():
     result = ["a", "b", "c"]
     return result
-
+print("make_abc")
 print(make_abc())
 
 
@@ -15,7 +17,7 @@ def equal_edges(items):
         return True
     else:
         return False
-    
+print("equal_edges")
 print(equal_edges([1, 2, 4, 1]))
 print(equal_edges([1, 2, 4, 5]))
 
@@ -33,7 +35,7 @@ def common_edge(list1, list2):
         return False
     
 
-    
+print("Common_edge")   
 print(common_edge([1, 2, 3], [3, 2, 1]))
 
 
@@ -54,7 +56,7 @@ def all_the_same(list1):
     else:
         return False
     
-
+print("all_the_same")
 print(all_the_same([5, 5, 5]))
 print(all_the_same([1, 2, 3]))
 
@@ -67,12 +69,12 @@ def all_unique(list1):
     first = list1[0]
     middle = list1[1]
     last = list1[2]
-    if first != middle and first != last:
+    if first != middle and first != last and middle != last:
         return True
     else:
         return False
     
-
+print("all_unique")
 print(all_unique([5, 5, 5]))
 print(all_unique([6, 7, 9]))
 
@@ -88,6 +90,90 @@ def increasing(list1):
     else:
         return False
     
-
+print("increasing")
 print(increasing([1, 2, 3]))
+
+
+
+def all_true(list1):
+    first = list1[0]
+    middle = list1[1]
+    last = list1[2]
+
+    if first == True and middle == True and last == True:
+        return True 
+    else:
+        return False
+    
+print("all_true")
+print(all_true([True, True, True]))
+print(all_true([False, True, True]))
+
+
+def mostly_true(list1):
+    first = list1[0]
+    middle = list1[1]
+    last = list1[2]
+    if first and middle == True or first and last == True or middle and last == True:
+        return True
+    else:
+        return False
+    
+print("mostly_true")
+print(mostly_true([True, True, False]))
+print(mostly_true([False, False, True]))
+print(mostly_true([True, True, True]))
+
+
+
+
+print("make_copy")
+def make_copy(list1):
+      first = list1[0]
+      middle = list1[1]
+      last = list1[2]
+      return [first, middle, last]
+original_list = [1, 2, 3]
+new_list = make_copy(original_list)
+print(original_list)
+print(new_list)
+
+print("repeat_thrice")
+def repeat_thrice(number):
+    return [number, number, number]
+
+print(repeat_thrice([1]))
+print(repeat_thrice([6]))
+
+
+print("make_reversed_copy")
+
+def make_reversed_copy(list1):
+    first = list1[0]
+    middle = list1[1]
+    last = list1[2]
+    return [last, middle, first]
+
+original_list1 = [5, 3, 0]
+new_list1 = make_reversed_copy(original_list1) 
+print(original_list1)
+print(new_list1)
+
+
+
+def reverse_in_place(list1):
+    numbers = list1.copy()
+    list1[0] = numbers[2]
+    list1[2] = numbers[0]
+
+    return list1
+
+print("reverse_in_place")
+original_list2 = [random.randint(1,9), random.randint(1,9), random.randint(1,9)]
+print(reverse_in_place(original_list2))
+print(original_list2)
+
+
+
+
 
---
+++ 06ListsAndLoops/Assignment/main.py
@@ -0,0 +1,182 @@
+def count_failing_grades(grades):
+    count = 0
+    for grade in grades:
+        if grade < 60:
+            count = count + 1
+    return count
+        
+print("count_failing_grades")
+inputlist = [80, 70, 52, 40, 60]
+returnvalue = count_failing_grades(inputlist)
+print(returnvalue)
+
+
+def count_act_scores(scores):
+    count = 0
+    for score in scores:
+        if score <= 36 and score >= 1:
+            count = count + 1
+    return count
+
+print("cout_act_scores")
+inputlist = [1, 26, 12, 9, 90]
+returnValue = count_act_scores(inputlist)
+print(returnValue)
+
+
+
+def number_sum(numbers):
+    total = 0
+    for number in numbers:
+       total = total + number
+    return total
+
+print("number_sum")
+inputlist=[3, 7, 5]
+returnValue = number_sum(inputlist)
+print(returnValue)
+
+
+def average_act_score(acts):
+    count = 0
+    total = 0
+    for act in acts:
+        if act <= 36 and act >= 1:
+           total = total + act
+           count = count + 1
+    if count == 0:
+        return None
+    return total / count
+
+print("average_act_score")
+inputlist=[1, 26, 90, 34, 18, 5]
+returnValue = average_act_score(inputlist)
+print(returnValue)
+
+
+
+def all_true(booleans):
+    for boolean in booleans:
+        if boolean not in [True]:
+            return False
+    return True
+print("all_true")
+print(all_true([True, True, True, True]))
+print(all_true([False, True, False]))
+
+
+
+
+def any_true(booleans):
+    for boolean in booleans:
+        if boolean in [True]:
+            return True
+    return False
+print("any_true")
+print(any_true([True, False, False]))
+print(any_true([False, False, False]))
+
+
+def has_vowel(letters):
+    for letter in letters:
+        if letter in ["a", "e", "i", "o", "u"]:
+            return True
+    return False
+
+
+print("has_vowel")
+print(has_vowel(["a", "s", "g", "c"]))
+print(has_vowel(["d", "s", "c", "q"]))
+
+
+
+def mostly_true(Booleans):
+    count = 0
+    countF = 0
+    for Boolean in Booleans:
+        if Boolean == True:
+            count += 1
+        if Boolean == False:
+            countF += 1
+    if count > countF:
+        return True
+    return False
+print("mostly_true")
+print(mostly_true([True, True, False]))
+print(mostly_true([False, False, True]))
+
+
+
+
+def all_the_same(nums):
+    first = nums[0]
+    for num in nums:
+        if num != first:
+            return False
+    return True
+
+
+    # repetition = False
+    # for num in nums:
+    #     if num[0] == num[1] and num[0] == num[2] and num[0] ==
+    #     if num == nums[0]:
+    #         return True
+    #     else:
+    #         return False   
+print("all_the_same")
+print(all_the_same([5, 5, 5, 5]))
+print(all_the_same([6, 2, 4, 5])) 
+
+
+
+
+def sum_with_skips(nums):
+    total = 0
+    ignoring = False
+
+    for num in nums:
+        if ignoring == True:
+            if num == -1:
+                ignoring = False
+        elif ignoring == False:
+            if num == -1:
+                ignoring = True
+            else:
+                total = total + num
+        print(num, ignoring)
+    return total
+
+print("sum_with_skips")
+print(sum_with_skips([2, 1, -1, 4, -1, 9]))
+
+
+
+
+
+def increasing(nums):
+    first = nums[0]
+    previous = nums[0]-1
+    for num in nums:
+        if num <= previous:
+            return False
+        previous = num
+    return True
+
+
+print("increasing")
+print(increasing([1, 2, 3, 4,]))
+print(increasing([1, 2, 3, 2, 4, 5]))
+print(increasing([5, 5, 6, 8, 9]))
+
+
+
+
+
+
+def is_incrementing(nums):
+    previous = nums[0]-1
+    for num in nums:
+        if num <= previous:
+            return False
+        previous = num
+    return True
